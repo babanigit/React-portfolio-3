c082da4b78036268241fdca6fdf317dfd8ac2c39
Aniket Panchal <aniketpanchal07@gmail.com>
2023-09-05 01:19:13 +0530

    commiting

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import "./App.css";
+import React from "react";
+
+import ReactLogo from "./components/ReactLogo";
+
+function App() {
+  return (
+    <>
+      <ReactLogo />
+    </>
+  );
+}
+
+export default App;
