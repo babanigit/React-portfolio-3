618d67c4ab3b0055a05e9ad67699d6dc74ba55a9
Aniket Panchal <aniketpanchal07@gmail.com>
2023-12-03 20:28:08 +0530

    edited

--- src/components/notFound/Notfound.jsx
+++ src/components/notFound/Notfound.jsx
@@ -4,13 +4,19 @@ import React from 'react'
 const Notfound = () => {
   return (
     <div>
-      <p>404 page not found</p>
+      {/* <p>404 page not found</p> */}
 
-      
+      <div className="flex items-center justify-center h-screen w-screen bg-gray-200">
+        <div className="text-center">
+          <h1 className="text-6xl font-bold text-red-500 mb-4">404</h1>
+          <p className="text-xl text-black">Oops! Page not found.</p>
+          {/* You can add more content or links here */}
+        </div>
+      </div>
 
-      
+
     </div>
-  )
+  );
 }
 
 export default Notfound
